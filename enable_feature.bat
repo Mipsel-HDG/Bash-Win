1c523df015ddb8b08d7cf448e90dd34400d9b2ad
Dan Siebels <30774292+Mipsel-HDG@users.noreply.github.com>
2017-08-28 10:05:05 -0700

    Update enable_feature.bat

--- enable_feature.bat
+++ enable_feature.bat
@@ -11,14 +11,15 @@ color 09
 
 echo "<Enabling Windows Bash Beta>"
 
+color 07
+
 DISM /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux
 pause
 
-echo "Windows Must be restarted to Complete Install Bash!"
-echo "After your Computer Comes Back From a Restart, Search for <'bash'> In your search!"
+echo "Starting lxrun /install /y - (Install GNUBash)"
 pause
 
-GOTO Initiate_Reboot
+GOTO Initiate_Install
 
-:Initiate_Reboot
-shutdown.exe /r /t 00
+:Initiate_Install
+lxrun /install /y
